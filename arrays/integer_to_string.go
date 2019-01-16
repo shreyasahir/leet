a46536cd788eb9babcd19fe294c62a78935d0fb6
Shreyas Ahir <shreyas.ahir@gmail.com>
2019-01-16 03:43:16 -0800

    atoi

---
+++ arrays/integer_to_string.go
@@ -0,0 +1,127 @@
+package main
+
+import "fmt"
+
+func main() {
+	input := "    -42"
+	fmt.Println("myatoi", myAtoi(input))
+}
+
+const (
+	maxInt32 = 1<<31 - 1
+	minInt32 = -1 << 31
+)
+
+// func myAtoi(str string) int {
+// 	var out int64
+// 	// isParsing marked as we're parsing the number.
+// 	// isParsing is set to true whenever we encountered the sign (+/-) or digits rune (0..9).
+// 	// Note: if isParsing == true, then all runes will be discarded, except digits rune (0-9).
+// 	sign, isParsing := "", false
+
+// 	for _, r := range str {
+// 		if r == ' ' {
+// 			if isParsing { // "  1", " 1 123"
+// 				break
+// 			}
+// 			continue
+// 		}
+// 		if r == '+' || r == '-' {
+// 			if isParsing || sign != "" { // "--0", "++0", "1-123"
+// 				break
+// 			}
+// 			isParsing = true
+// 			sign = string(r)
+// 			continue
+// 		}
+// 		if !(r >= 48 && r <= 57) { // If rune also not the digit then stop parsing
+// 			break
+// 		}
+
+// 		isParsing = true
+// 		out *= 10
+// 		out += int64(r - 48)                     // Convert digit's code point to its actual integer value
+// 		if out >= maxInt32 || -out <= minInt32 { // Stop parsing when overflow
+// 			break
+// 		}
+// 	}
+
+// 	if sign == "-" {
+// 		out = -out
+// 		if out < minInt32 {
+// 			return minInt32
+// 		}
+// 	}
+// 	if out > maxInt32 {
+// 		return maxInt32
+// 	}
+// 	return int(out)
+// }
+
+func myAtoi(str string) int {
+
+	var newStr, result string
+
+	count := 0
+	for _, v := range str {
+		// fmt.Println("v", v)
+		if v != 32 && v < 97 {
+			newStr += string(v)
+		} else {
+			if v == 32 {
+				continue
+			} else if count == 0 {
+				return 0
+			}
+			continue
+		}
+		count++
+	}
+
+	var sign bool
+	count = 0
+	// fmt.Println("newStr", newStr)
+
+	for _, v := range newStr {
+		if string(v) == "-" && count == 0 {
+			sign = true
+		} else {
+			if v < '0' || v > '9' {
+				return 0
+			} else {
+				result += string(v)
+			}
+
+		}
+		count++
+	}
+
+	// fmt.Println("result", result)
+
+	var d rune
+	var n int64
+	for _, v := range result {
+		if '0' <= v && v <= '9' {
+			d = v - '0'
+		}
+		n *= 10
+		n += int64(d)                        // Convert digit's code point to its actual integer value
+		if n >= maxInt32 || -n <= minInt32 { // Stop parsing when overflow
+			break
+		}
+	}
+
+	if sign {
+		n *= -1
+
+		if n < minInt32 {
+			return minInt32
+		}
+	}
+
+	if n > maxInt32 {
+		return maxInt32
+	}
+	return int(n)
+
+}
--- arrays/longestpalindrome.go
+++ arrays/longestpalindrome.go
@@ -1,30 +1,46 @@
 package main
 
-import (
-	"fmt"
-)
-
-// func longestPalindrome(s string) {
-// 	//start := 0
-// 	end := len(s) - 1
-// 	//count := 0
-// 	for i := 0; i <= end/2; i++ {
-// 		fmt.Println("i,mid,end", i, end/2, end-i)
-// 		fmt.Println("left,right", s[i:end/2], s[end/2:end+1-i])
-
-// 		if s[i:end/2] == s[end/2:end+1-i] {
-// 			fmt.Println("palindrome", s[i:end+1-i])
-// 			//count += 2
-// 		}
-// 	}
-// 	//print(count)
-// 	//return count
-// }
+import "fmt"
 
 func longestPalindrome(s string) string {
 
+	if s == "" {
+		return ""
+	}
+	if len(s) == 1 {
+		return s
+	}
+
+	longest := 1
+	var str string
+	str = string(s[0])
+	n := len(s)
+
+	for i := 0; i < n; i++ {
+		tmp := helper(s, i, i)
+		if len(tmp) > longest {
+			longest = len(tmp)
+			str = tmp
+		}
+		fmt.Println("tmp", tmp)
+		tmp = helper(s, i, i+1)
+		if len(tmp) > longest {
+			longest = len(tmp)
+			str = tmp
+		}
+		fmt.Println("tmp", tmp)
+
+	}
+	return str
 }
 
+func helper(s string, begin int, end int) string {
+	for begin >= 0 && end < len(s) && s[begin] == s[end] {
+		begin--
+		end++
+	}
+	return s[begin+1 : end]
+}
 func main() {
-	longestPalindrome("cbbd")
+	fmt.Println(longestPalindrome("ac"))
 }
