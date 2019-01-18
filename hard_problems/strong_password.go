50872000b5da88dca5ba3e0945fb0b8c255006c8
shreyas ahir <ahirs@vmware.com>
2019-01-18 15:26:44 -0800

    strong password problem

--- hard_problems/strong_password.go
+++ hard_problems/strong_password.go
@@ -7,7 +7,86 @@
 
 // Insertion, deletion or replace of any one character are all considered as one change.
 // https://leetcode.com/problems/strong-password-checker/
+// http://massivealgorithms.blogspot.com/2016/10/leetcode-420-strong-password-checker.html
 
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	password := "aoveeA1983"
+	fmt.Println("strongPasswordChecker", strongPasswordChecker(password))
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min(a, b int) int {
+	if a > b {
+		return b
+	}
+	return a
+}
 func strongPasswordChecker(s string) int {
+	requiredChar := getRequiredChar(s)
+
+	fmt.Println("requiredchar", requiredChar)
+	if len(s) < 6 {
+		return max(requiredChar, 6-len(s))
+	}
+
+	replace := 0
+	oned := 0
+	twod := 0
+
+	for i := 0; i < len(s); {
+		length := 1
+		for i+length < len(s) && s[i+length] == s[i+length-i] {
+			length++
+		}
+		if length >= 3 {
+			replace += length / 3
+			if length%3 == 0 {
+				oned++
+			}
+			if length%3 == 1 {
+				twod++
+			}
+		}
+		i += length
+	}
+
+	if len(s) <= 20 {
+		return max(requiredChar, replace)
+	}
+	deleteCount := len(s) - 20
+
+	replace -= min(deleteCount, oned)
+	replace -= min(max(deleteCount-oned, 0), twod) / 2
+	replace -= max(deleteCount-oned-twod, 0) / 3
+
+	return deleteCount + max(requiredChar, replace)
+}
+
+func getRequiredChar(s string) int {
+	lowercase, uppercase, digit := 1, 1, 1
+
+	for _, v := range s {
+		if 0 <= int(v-'0') && int(v-'0') <= 9 {
+			digit = 0
+		} else if v >= 'a' && v <= 'z' {
+			lowercase = 0
+		} else if v >= 'A' && v <= 'Z' {
+			uppercase = 0
+		}
+	}
+	return lowercase + digit + uppercase
 
 }
