ca33aefe808f8a368c434d9d28386d6a6e08e557
Shreyas Ahir <shreyas.ahir@gmail.com>
2019-02-08 12:55:54 -0800

    range and max bst tree

--- trees/maximum_tree.go
+++ trees/maximum_tree.go
@@ -1,1 +1,59 @@
 //https://leetcode.com/problems/maximum-binary-tree/
+
+package main
+
+import (
+	"fmt"
+)
+
+// * type TreeNode struct {
+// 	*     Val int
+// 	*     Left *TreeNode
+// 	*     Right *TreeNode
+// 	* }
+
+func main() {
+	arr := []int{3, 2, 1, 6, 0, 5}
+	max, k := sortArray(arr)
+	left := arr[:k]
+	right := arr[k+1:]
+	fmt.Println("max", max)
+	fmt.Println("left", left)
+	fmt.Println("right", right)
+}
+
+func constructMaximumBinaryTree(nums []int) *TreeNode {
+
+	var tnode TreeNode
+	var maxk int
+
+	if len(nums) == 0 {
+		return nil
+	}
+
+	maxk = findMaxIndex(nums)
+	tnode.Val = nums[maxk]
+	if nums[:maxk] != nil {
+		tnode.Left = constructMaximumBinaryTree(nums[:maxk])
+	} else {
+		tnode.Left = nil
+	}
+
+	if nums[maxk+1:] != nil {
+		tnode.Right = constructMaximumBinaryTree(nums[maxk+1:])
+	} else {
+		tnode.Right = nil
+	}
+
+	return &tnode
+}
+
+func findMaxIndex(slice []int) int {
+	index := 0
+	for i, j := range slice {
+		if j > slice[index] {
+			index = i
+		}
+	}
+	return index
+}
--- trees/range_sum_bst.go
+++ trees/range_sum_bst.go
@@ -1,1 +1,42 @@
 // https://leetcode.com/problems/range-sum-of-bst/
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+var (
+	result []int
+)
+
+func rangeSumBST(root *TreeNode, L int, R int) int {
+	if root == nil {
+		return 0
+	}
+	result = nil
+	var sum int
+	inOrder(root)
+	//fmt.Println("result",result)
+	if len(result) > 0 {
+		for i := 0; i < len(result); i++ {
+			if result[i] >= L && result[i] <= R {
+				sum += result[i]
+			}
+		}
+	}
+
+	return sum
+}
+
+func inOrder(n *TreeNode) {
+	if n != nil {
+		inOrder(n.Left)
+		//f(n)
+		result = append(result, n.Val)
+		inOrder(n.Right)
+	}
+}
